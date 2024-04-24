fcbd8ca3ff432e8799ca57b19fb0bff37e6931ef
Atul Tingre <atultingre.work@gmail.com>
2024-04-24 14:33:30 +0530

    Remove food

--- controllers/food.controllers.js
+++ controllers/food.controllers.js
@@ -36,6 +36,17 @@ const listFood = async (req, res) => {
 
 const updateFood = async (req, res) => {};
 
-const deleteFood = async (req, res) => {};
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food?.image}`, () => {});
 
-export { addFood, listFood, updateFood, deleteFood };
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Food Removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { addFood, listFood, updateFood, removeFood };
--- routes/food.routes.js
+++ routes/food.routes.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { addFood, listFood } from "../controllers/food.controllers.js";
+import {
+  addFood,
+  listFood,
+  removeFood,
+} from "../controllers/food.controllers.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
@@ -17,5 +21,6 @@ const upload = multer({ storage: storage });
 
 foodRouter.post("/add", upload.single("image"), addFood);
 foodRouter.get("/list", listFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
