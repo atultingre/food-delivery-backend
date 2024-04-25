f6a486d47af1e94f8c25469dc1d2b10a65206fd0
Atul Tingre <atultingre.work@gmail.com>
2024-04-25 13:33:33 +0530

    Login user

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -4,7 +4,28 @@ import validator from "validator";
 import userModel from "../models/user.model.js";
 
 // login user
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User doen't exists" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+
+    const token = createToken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, error });
+
+    toast.error(error);
+  }
+};
 
 // create token
 const createToken = (id) => {
