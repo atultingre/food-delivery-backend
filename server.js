13cdd706106811660ddce3fe37c2c6079df47047
Atul Tingre <atultingre.work@gmail.com>
2024-04-26 02:04:34 +0530

    Add item to cart

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/food.routes.js";
 import userRouter from "./routes/user.routes.js";
+import cartRouter from "./routes/cart.routes.js";
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ connectDB();
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
 app.listen(port, () =>
   console.log(`server is running on http://localhost:${port}`)
